300a50d8f3c50d337616f77a864fb2a8ad9831cb
Mark Ferree <markferree@gmail.com>
2016-07-01 11:16:40 -0700

    One step closer still can't get characters unquoted

--- 30days/30-review-loop/main.go
+++ 30days/30-review-loop/main.go
@@ -3,26 +3,32 @@ package main
 import "fmt"
 import "bufio"
 import "os"
+import "strconv"
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	myString := scanner.Text()
-	var even []rune
-	var odd []rune
-	for i, r := range myString {
-		if i%2 == 0 {
-			even = append(even, r)
-		} else {
-			odd = append(odd, r)
+	scanner.Scan() // Skip number of strings
+	for scanner.Scan() {
+		myString := scanner.Text()
+		var even []rune
+		var odd []rune
+		for i, r := range myString {
+			if i%2 == 0 {
+				even = append(even, r)
+			} else {
+				odd = append(odd, r)
+			}
 		}
-	}
 
-	for _, character := range even {
-		fmt.Printf("%q", character)
-	}
-	fmt.Println(" ")
-	for _, character := range odd {
-		fmt.Printf("%q", character)
+		for _, character := range even {
+			s := strconv.QuoteRune(character)
+			fmt.Print(string(s))
+		}
+		fmt.Printf(" ")
+		for _, character := range odd {
+			s := strconv.QuoteRune(character)
+			fmt.Print(string(s))
+		}
+		fmt.Println()
 	}
 }
