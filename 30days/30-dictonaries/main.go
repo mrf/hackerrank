3e8e1d0a03bfa84099c5c5ff9bc3d05c38c1ed44
Mark Ferree <markferree@gmail.com>
2016-07-12 18:36:45 -0700

    Dictionaries ie maps

---
+++ 30days/30-dictonaries/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+import "bufio"
+import "os"
+import "strconv"
+import "strings"
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	var m map[string]int
+	m = make(map[string]int)
+	scanner.Scan()
+	length, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < length; i++ {
+		scanner.Scan()
+		pieces := strings.Fields(scanner.Text())
+		number, err := strconv.Atoi(pieces[1])
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		m[pieces[0]] = number
+	}
+	fmt.Println(m)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		search := scanner.Text()
+		if m[search] {
+
+		}
+	}
+}
